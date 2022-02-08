7cd88f1964ca4bd4e97608cbab0c631aa357010b
Luis Carlos de Souza <47360932+luiscsmuniz@users.noreply.github.com>
2022-02-08 13:49:49 +0000

    feat(bot): adicionado pra buscar o comando automaticamente

--- commands/loot.js
+++ commands/loot.js
@@ -1,9 +1,8 @@
 const lootUser = require('../data/loot.json')
 
-const loot = async (client, channel, username) => {
-  console.log(lootUser)
-  
-  return client.say(channel, `${username}, você achou um baú do tesouro! :moneybag: Você acaba de encontrar: ${lootUser.loot[Math.floor(Math.random()*lootUser.loot.length)]}`);
-}
-
-module.exports = loot
+module.exports = {
+  name: "loot",
+  execute(client, channel, tags) {
+    return client.say(channel, `${tags.username}, você achou um baú do tesouro! :moneybag: Você acaba de encontrar: ${lootUser.loot[Math.floor(Math.random()*lootUser.loot.length)]}`)
+  }
+};
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const tmi = require('tmi.js');
+const { readdirSync } = require("fs");
+const { join } = require("path");
 const loot = require('./commands/loot')
 const BOTNAME = 'Rleule'
 const CHANNELS = ['Valandiil', 'Wig1']
@@ -11,16 +13,32 @@ const opts = {
   },
   channels: CHANNELS,
 }
+
 const client = new tmi.client(opts);
+
+const commands = []
+
+const commandFiles = readdirSync(join(__dirname, "commands")).filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(join(__dirname, "commands", `${file}`));
+  commands.push(command);
+}
 
 client.connect();
 
-client.on('message', (channel, tags, message, self) => {
+client.on('message', async (channel, tags, message, self) => {
+
 	if(self) return;
+  const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
+  const [raw, commandName, argument] = message.match(regexpCommand);
 
-  const command = message.toLowerCase()
+  console.log({ raw, commandName, argument })
 
-	if(command === '!loot') {
-		return loot(client, channel, tags.username)
-	}
+  const command = commands.find(item => item.name === commandName)
+
+  try {
+    command.execute(client, channel, tags, argument);
+  } catch (error) {
+    console.error(error);
+  }
 });
